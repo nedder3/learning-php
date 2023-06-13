09a6f5262ccb651d1afdbecb84e119f543487514
nedder3 <arij.dev@gmail.com>
2023-06-13 01:15:12 -0300

    Add ejercicio 5: Vars

---
+++ ejercicio5.php
@@ -0,0 +1,13 @@
+<?php
+
+$Edad=32; //entero
+
+$nombre = "Oscar"; //cadena 
+
+$Altura=1.4; //flotante o decimal
+
+$Programador=null; // nulo
+
+echo "el nombre es ".$nombre." la edad es ".$Edad." la altura es : ".$Altura." estado programador : ".$Programador ;
+
+?>
