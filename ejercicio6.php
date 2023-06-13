9ecebd15272203816344dfc3c31934e05bbc0f4b
nedder3 <arij.dev@gmail.com>
2023-06-13 01:44:23 -0300

    Ejercicio 6 : constantes

---
+++ ejercicio6.php
@@ -0,0 +1,17 @@
+<?php 
+    $edad=32; //var
+
+    echo $edad."<br/>"; //puede incrustrar html
+
+    $edad=40; //cambio el valor 
+
+    echo $edad."<br/>";
+
+    //uso de la constante
+    define("NOMBRE","OSCAR");
+
+    // imprimir constante
+    echo NOMBRE;
+
+
+?>
