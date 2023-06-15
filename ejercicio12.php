123a1554e369ad4b71d85c06bee3411a6057e4ff
nedder3 <arij.dev@gmail.com>
2023-06-14 23:51:44 -0300

    Add ejercicio 12 : switch

---
+++ ejercicio12.php
@@ -0,0 +1,44 @@
+<?php
+    if($_POST){
+
+        $boton=$_POST['btnValor'];
+
+        switch($boton){
+            case 1:
+                echo"el usuario presiono el boton 1";
+                break;
+
+            case 2:
+                echo"el usuario presiono el boton 2";
+                break;
+
+            case 3:
+                 echo"el usuario presiono el boton 3";
+                 break;
+
+        }
+
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="ejercicio12.php" method="post">
+
+        <input type="submit" name="btnValor" value="1">
+        <br>
+        <input type="submit" name="btnValor" value="2">
+        <br>
+        <input type="submit" name="btnValor" value="3">
+
+    </form>
+
+</body>
+</html>
